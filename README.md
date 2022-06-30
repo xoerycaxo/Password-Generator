cf66661569011dd08df8c3e23acd6ca5cc27e0de
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-06-30 16:57:24 -0400

    assignment code done

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Password-Generator
+Purpose
+
+The purpose of this assignment is to design a [assword generator that generates passwords based on input and recieved from the user via window prompts.
+
+Built with 
+
+HTML
+CSS
+Javascript
+
+Web Site:
+
+
+Contribution 
+Made by Eryca Spence- Pierritz]
--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+var characterLength = 8;
+var passwordChoice = [];
+
+var randomSymbols = ['!', '@', '#', '^', '(', ')', '$', '%']; 
+var randomLower = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var randomUpper = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var randomNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+
+
+var generateBtn = document.querySelector("#generate"); // goes to generate button in HTML
 
 // Write password to the #password input
 function writePassword() {
